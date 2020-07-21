0a512bccfb23c4be0e224f19f9bb2f1a89dea6fa
Hoang Vu <hoang.vu@uafs.edu>
2020-07-21 18:43:27 -0500

    Updates

---
+++ precache-manifest.065110860a2bb9e412ec99e568b5db48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa31661c6f33260dcabf88748ec9615",
+    "url": "/navigation/index.html"
+  },
+  {
+    "revision": "b9ea6387b5e40e7d5000",
+    "url": "/navigation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "876c0fb4915f8bc5e1bc",
+    "url": "/navigation/static/js/2.b69e56f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/navigation/static/js/2.b69e56f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ea6387b5e40e7d5000",
+    "url": "/navigation/static/js/main.a5819a97.chunk.js"
+  },
+  {
+    "revision": "20b10aa853b8e70aabdf",
+    "url": "/navigation/static/js/runtime-main.509824a9.js"
+  }
+]);
